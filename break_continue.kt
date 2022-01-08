4632261d2ded637885335fd7f02cb2ea4b02611a
Rita Bindal <42319102+Rita63@users.noreply.github.com>
2022-01-08 20:08:34 +0530

    Create break_continue.kt

---
+++ break_continue.kt
@@ -0,0 +1,30 @@
+package com.example.kotlin
+
+fun main(){
+    /*for(num in 1..20) {
+    println("$num")
+
+        if(num==7){
+            break
+        }
+
+
+
+}
+    println("done with the loop")
+}*/
+
+//continue
+for(num in 1..20) {
+
+
+    if(num==7){
+        continue
+    }
+    println("$num")
+
+
+
+}
+
+}
