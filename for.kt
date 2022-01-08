c4ce4b359a34639884ffd866706e22173c2ce981
Rita Bindal <42319102+Rita63@users.noreply.github.com>
2022-01-08 19:13:11 +0530

    Create for.kt

---
+++ for.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin
+
+fun main(){
+    /*for(num in 1..20)
+        println("$num")
+}
+for(i in 10 downTo 1)
+    println("$i")
+}*/
+
+for(i in 1 until 10 step 2)
+    println("$i")
+}
