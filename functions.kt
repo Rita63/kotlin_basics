496788bfbe0e446d19b7611a6e7331a62ef836de
Rita Bindal <42319102+Rita63@users.noreply.github.com>
2022-01-08 22:40:46 +0530

    Create functions.kt

---
+++ functions.kt
@@ -0,0 +1,23 @@
+package com.example.kotlin
+
+//substraction of two number using function
+fun main(){
+    /* var result =subFunction(6,8) //arguments
+    println("result is $result")
+
+}
+fun subFunction(a:Int,b:Int):Int{  //parameters
+    return a-b
+
+}*/
+//average of two numbers using function
+
+
+    var average = avgFunction(3.0,7.9)
+    println("average is $average")
+}
+fun avgFunction(a:Double,b:Double):Double{   //we use double here because the average of two numbers will not return any whole number
+return (a+b)/2
+}
+
+//method- it is a function within the class
