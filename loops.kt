b0a6ab18e5012f1da5da1db12e3eb32542963273
Rita Bindal <42319102+Rita63@users.noreply.github.com>
2022-01-08 19:01:01 +0530

    Create loops.kt

---
+++ loops.kt
@@ -0,0 +1,18 @@
+package com.example.kotlin
+//while loop
+fun main() {
+    /*var y =1
+    while(y<=10){
+        println("$y")
+    y++}
+
+}*/
+//do-while loop
+
+    var x = 1
+    do {
+        println("$x")
+        x++
+
+    } while (x <= 5)
+}
