01e834ee244dc5f4de86a18e3602a893a7978dc0
Rita Bindal <42319102+Rita63@users.noreply.github.com>
2022-01-09 21:40:30 +0530

    Update class.kt

--- class.kt
+++ class.kt
@@ -13,3 +13,26 @@ class Person (firstName: String="Rita",lastName:String="Bindal")  //creating a c
     }
 
 }
+
+
+//example
+//Please create a class called MobilePhone. It should have three primary constructor variables, osName, brand and model.
+//
+//Use the initializer to print those details.
+//
+//Create three objects of that class in the Main function.
+//
+//Examples of phones would be:
+//Samsung Galaxy S20 Ultra. Here the osName is Android, brand is Samsung, model is Galaxy S20 Ultra
+
+fun main(){
+    var samsung=MobilePhone("Android","Samsung","Galaxy S20 Ultra")
+    var lenovo=MobilePhone()
+    var oppo=MobilePhone(brand = "oppo",model = "ultra max")
+}
+class MobilePhone(osName:String="android",brand:String="lenovo",model:String="max"){
+
+init{
+
+    println("phone details are " +"os name = $osName brand is= $brand model is= $model"  )
+}}
