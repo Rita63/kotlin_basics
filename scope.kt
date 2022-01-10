dd4f94a4b6a7df4b7a3cf3e379d480429b7fb7c2
Rita Bindal <42319102+Rita63@users.noreply.github.com>
2022-01-10 16:36:34 +0530

    Create scope.kt

---
+++ scope.kt
@@ -0,0 +1,14 @@
+package com.example.kotlin
+
+fun main(){
+myFunction(68)
+}
+fun myFunction(a:Int)  //the a here is parameter
+{
+  //  a = 5        //we cannot reassigned the value of a
+
+    //the a here is variable
+    var a = 5     //we can assign the value like this
+    print("value of a is $a")
+
+}
